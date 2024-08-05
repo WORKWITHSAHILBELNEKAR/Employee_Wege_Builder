64437453a4b629cf17edeed279755f0a63dd40b0
sahil belnekar <sahilsbelnekar2002@gmail.com>
2024-08-05 15:10:23 +0530

    added file employee_wage

---
+++ src/UC2_EmplyeeWage.java
@@ -0,0 +1,8 @@
+public class UC2_EmplyeeWage {
+    public static void main(String[] args) {
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+        int dailyWage = wagePerHour * fullDayHour;
+        System.out.println("Daily Employee Wage: $" + dailyWage);
+    }
+}
